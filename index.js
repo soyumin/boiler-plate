fde344d5fdeb064f4e7e9cac62c7ca45fcfe749a
소유민 <soyumin@soyumin-ui-MacBookPro.local>
2020-04-03 16:24:23 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://soyumin:asdf1234@boilerplate-r9oqv.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => console.log("MomgoDB Connected..."))
+  .catch(err => console.log(err));
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
